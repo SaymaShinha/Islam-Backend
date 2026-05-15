9e04e2ab51d4c1dbe17e80e66c4931379217325c
SaymaShinha <sayma.shinha@gmail.com>
2026-05-15 20:49:41 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,70 @@
+import express from "express";
+import cors from "cors";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is working");
+});
+
+app.post("/send", async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: process.env.EMAILTO,
+      subject: "New Contact Form Islam",
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    });
+
+    res.status(200).json({ success: true });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
+    res.status(500).json({ success: false });
+  }
+});
+
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Example: simple check (replace with DB)
+  if (email !== "test@gmail.com" || password !== "1234") {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(
+    { email },
+    "SECRET_KEY",
+    { expiresIn: "1h" }
+  );
+
+  res.json({ token });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
